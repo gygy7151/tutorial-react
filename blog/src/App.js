8cf6dff5b4a1c4dbbe7e640b2670c4c4569d2ee4
gygy7151 <gygy7151@gmail.com>
2022-09-28 09:41:46 +0900

    set state and enviroment

---
+++ blog/src/App.js
@@ -0,0 +1,55 @@
+/* eslint-disable*/
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+	let post = "강남 우동 맛집";
+	let [글제목, 글제목변경] = useState([
+		"남자 코트 추천",
+		"여자 코트 추천",
+		"아동복 코트 추천",
+	]);
+	let [따봉, 따봉변경] = useState(0);
+
+	return (
+		<div className="App">
+			<div className="black-nav">
+				<h4>블로그임</h4>
+			</div>
+			<div className="list">
+				<h4>
+					{글제목[0]}{" "}
+					<span
+						onClick={() => {
+							따봉변경(따봉 + 1);
+						}}
+					>
+						👍
+					</span>{" "}
+					{따봉}{" "}
+					<button
+						onClick={() => {
+							let copy = [...글제목];
+							copy[0] = "중년 코트 추천";
+							글제목변경(copy);
+						}}
+					>
+						글수정
+					</button>
+				</h4>
+				<p>2월 17일 발행</p>
+			</div>
+			<div className="list">
+				<h4>{글제목[1]}</h4>
+				<p>2월 17일 발행</p>
+			</div>
+			<div className="list">
+				<h4>{글제목[2]}</h4>
+				<p>2월 17일 발행</p>
+			</div>
+		</div>
+	);
+}
+
+export default App;
