a30a2183fc5540590bfad971b56e89a96e9ef926
gygy7151 <gygy7151@gmail.com>
2022-09-29 01:56:51 +0900

    feat: how to make component and when to use it

--- blog/src/App.js
+++ blog/src/App.js
@@ -48,8 +48,24 @@ function App() {
 				<h4>{글제목[2]}</h4>
 				<p>2월 17일 발행</p>
 			</div>
+			<Modal></Modal>
+			<Copyright />
 		</div>
 	);
 }
+
+function Modal() {
+	return (
+		<div className="modal">
+			<h4>제목</h4>
+			<p>날짜</p>
+			<p>상세내용</p>
+		</div>
+	);
+}
+
+const Copyright = () => {
+	return "bluevulpe";
+};
 
 export default App;
